7dcd2dfe9199a46a597b6179e78513e3e3b67549
ashwin rao <ashwinvdrk@gmail.com>
2021-03-19 21:14:29 +0530

    lets chat website

---
+++ precache-manifest.9bdebd137b422dc5c8b22109071f566c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48763b83f84419e4525b0a3017ab683d",
+    "url": "/letschatui/index.html"
+  },
+  {
+    "revision": "5b859767aa4f75b53aed",
+    "url": "/letschatui/static/css/main.eb83e224.chunk.css"
+  },
+  {
+    "revision": "85f27723ddca11a3fafb",
+    "url": "/letschatui/static/js/2.03af0605.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/letschatui/static/js/2.03af0605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b859767aa4f75b53aed",
+    "url": "/letschatui/static/js/main.8e7fa6b0.chunk.js"
+  },
+  {
+    "revision": "2a8bd97cb402f3137a32",
+    "url": "/letschatui/static/js/runtime-main.d2327368.js"
+  }
+]);
